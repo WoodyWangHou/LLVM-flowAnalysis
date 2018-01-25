75df91d798343e41e71455e368d6818a4726deb6
Hou Wang <wanghou92@gmail.com>
2018-01-24 23:51:24 -0800

    Complete CDI

--- Passes/Passes/part1/CountDynamicInstructions.cpp
+++ Passes/Passes/part1/CountDynamicInstructions.cpp
@@ -1,8 +1,9 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalVariable.h"
 #include "llvm/Support/raw_ostream.h"
 #include "/lib231/lib231.cpp"
-#include "llvm/Support/IRBuilder.h"
+#include "llvm/IR/IRBuilder.h"
 
 using namespace llvm;
 namespace{
@@ -10,24 +11,66 @@ namespace{
     static char ID;
     CountDynamicInstructions() : FunctionPass(ID){}
     bool runOnFunction(Function &F) override{
-    std::map<BasicBlock*, int*> BB_Op_map;
+    std::map<BasicBlock*, unsigned*> BB_Op_map;
     // prepare basic block instruction set
         for(BasicBlock &BB : F){
-          int* instr_set = new int[65];
+          unsigned* instr_set = new unsigned[65];
           for(Instruction &I : BB){
             unsigned name = I.getOpcode();
             instr_set[name]++;
           }
           // for(int i = 0; i < 65; i++){
-          //   errs() << mapCodeToName(i) << " : " << instr_set[i] << '\n';
+          //   errs()<< mapCodeToName(i) << ":" << instr_set[i] << '\n';
           // }
+          // errs() << "========================"<<'\n';
           BB_Op_map.insert({&BB,instr_set});
         }
         // insert block scanner into IR
-        for(BasicBlock &BB : F){
-
+        unsigned* instr_code = new unsigned[65];
+        for(int i = 0; i < 65; i++){
+          instr_code[i] = i;
         }
 
+        LLVMContext &context = F.getContext();
+        ArrayType* arrayTy = ArrayType::get(IntegerType::get(context, 32), 65);
+        GlobalVariable* instrCode = new GlobalVariable(*(F.getParent()),arrayTy,true,
+        GlobalValue::InternalLinkage,ConstantDataArray::get(context, *(new ArrayRef<uint32_t>(instr_code, 65))),
+        "instruction_codes");
+
+        for(BasicBlock &BB : F){
+          unsigned* data = BB_Op_map.at(&BB);
+          GlobalVariable* counts = new GlobalVariable(*(F.getParent()),arrayTy,true,
+          GlobalValue::InternalLinkage,ConstantDataArray::get(context, *(new ArrayRef<uint32_t>(data, 65))),
+          "instruction_count");
+
+          Function *updateFunc = cast<Function>(F.getParent()->getOrInsertFunction(
+            "updateInstrInfo",                           // name of function
+            Type::getVoidTy(context),        // return type
+            Type::getInt32Ty(context),
+            Type::getInt32PtrTy(context),       // first parameter type
+            Type::getInt32PtrTy(context)       // second parameter type
+          ));
+
+          IRBuilder<> Builder(&*(BB.getFirstInsertionPt())); // init IRBuilder
+          Value* sizePtr = ConstantInt::get(IntegerType::get(context,32), 65);
+          Value* instrCodePtr = Builder.CreatePointerCast(instrCode, Type::getInt32PtrTy(context));
+          Value* countsPtr = Builder.CreatePointerCast(counts, Type::getInt32PtrTy(context));
+          std::vector<Value*> args;
+          args.push_back(sizePtr);
+          args.push_back(instrCodePtr);
+          args.push_back(countsPtr);
+          Builder.CreateCall(updateFunc, args);
+          // if the block contains return
+          std::map<BasicBlock*, unsigned*>::iterator it = BB_Op_map.find(&BB);
+          if(it->second[1] != 0){
+            // if there is a return in the block, print the result
+            Function *printFunc = cast<Function>(F.getParent()->getOrInsertFunction(
+              "printOutInstrInfo",                           // name of function
+              Type::getVoidTy(context)        // return type
+            ));
+            Builder.CreateCall(printFunc);
+          }
+        }
     }
   };
 }
--- Tests/test-example/run.sh
+++ Tests/test-example/run.sh
@@ -16,13 +16,13 @@ $LLVM_BIN/clang++ -c $TEST_DIR/test1-main.cpp -emit-llvm -S -o /tmp/test1-main.l
 
 $LLVM_BIN/opt -load $LLVM_SO/CSE231.so -cse231-csi < /tmp/test1-c.ll > /dev/null 2> /tmp/csi.result
 $LLVM_BIN/opt -load $LLVM_SO/CSE231.so -cse231-cdi < /tmp/test1.ll -o /tmp/test1-cdi.bc
-$LLVM_BIN/opt -load $LLVM_SO/CSE231.so -cse231-bb < /tmp/test1.ll -o /tmp/test1-bb.bc
+#$LLVM_BIN/opt -load $LLVM_SO/CSE231.so -cse231-bb < /tmp/test1.ll -o /tmp/test1-bb.bc
 
 $LLVM_BIN/llvm-dis /tmp/test1-cdi.bc
-$LLVM_BIN/llvm-dis /tmp/test1-bb.bc
+#$LLVM_BIN/llvm-dis /tmp/test1-bb.bc
 
 $LLVM_BIN/clang++ /tmp/lib231.ll /tmp/test1-cdi.ll /tmp/test1-main.ll -o /tmp/cdi_test1
-$LLVM_BIN/clang++ /tmp/lib231.ll /tmp/test1-bb.ll /tmp/test1-main.ll -o /tmp/bb_test1
+#$LLVM_BIN/clang++ /tmp/lib231.ll /tmp/test1-bb.ll /tmp/test1-main.ll -o /tmp/bb_test1
 
 /tmp/cdi_test1 2> /tmp/cdi.result
 /tmp/bb_test1 2> /tmp/bb.result
